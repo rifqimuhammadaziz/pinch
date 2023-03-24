fd688d98ceee34242296690f731907e94a1de60f
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-03-24 11:45:15 +0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Rifqi Muhammad Aziz on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
